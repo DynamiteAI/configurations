ef317d52b693c40e33ace0dbd281247d5c3d9c4a
JaminB <jamin@dynamite.ai>
2021-08-10 20:10:21 -0400

    fixed permissions

--- base/default_configs/zeek/uncompiled_scripts/zeek-community-id/build/CMakeFiles/bif-init-Corelight_CommunityID.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "__bro_plugin__"
-  "CMakeFiles/bif-init-Corelight_CommunityID"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/bif-init-Corelight_CommunityID.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- base/default_configs/zeek/uncompiled_scripts/zeek-community-id/build/lib/bif/__load__.zeek
+++
@@ -1,1 +0,0 @@
-@load ./communityid.bif.zeek
